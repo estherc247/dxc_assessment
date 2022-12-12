e22c3518d72e9841656ecb918803731a5a02788d
estherc247 <61535683+estherc247@users.noreply.github.com>
2022-12-12 15:39:58 +0800

    Update InputData.java

--- InputData.java
+++ InputData.java
@@ -2,20 +2,23 @@ public class InputData {
     private String plainText;
     private String encodedString;
     private String ofsValue; 
-
+    
+    //default
     public InputData(){}
-
-    public InputData(String plainText, String encodedString, String ofsValue){
+    
+    //update based on user input 
+    public void encoderData(String plainText, String ofsValue){
         this.plainText = plainText;
-        this.encodedString = encodedString;
         this.ofsValue = ofsValue;
     }
-    public void setPT(String plainText){this.plainText = plainText;}
-    public void setES(String encodedString){this.encodedString = encodedString;}
-    public void setOffset(String ofsValue){this.ofsValue = ofsValue;}
-
+    
+    public void decoderData(String encodedString) {
+    	this.encodedString = encodedString;
+    }
+    
+    //Call updated values 
     public String getPT(){return plainText;}
-    public String getES(){return encodedString;}
     public String getOffset(){return ofsValue;}
+    public String getES(){return encodedString;}
     
 }

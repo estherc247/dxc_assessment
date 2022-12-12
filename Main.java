5f62158d3d730c996abd380acceed49db9b71ac1
estherc247 <61535683+estherc247@users.noreply.github.com>
2022-12-12 15:39:32 +0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,35 +3,62 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
-import javax.swing.plaf.synth.SynthStyleFactory;
-
 
 public class Main {
 
     public static void main(String[] args){
-
+    	
+    	String ofsValue = "";
+    	String plainText = "";
+    	String encodedString = "";
+    	int choice  = 1;
+    	
         EncoderDecoderService EDService = new EncoderDecoderService();
-
+        InputData newData =  new InputData();
+        
+        Scanner userChoice = new Scanner(System.in);
         Scanner userInput = new Scanner(System.in);
-        System.out.print("\n String to Encode: ");
-        String plainText = userInput.nextLine();
-
-        System.out.println("\n String to Decode: ");
-        String encodedString = userInput.nextLine();
-
-        System.out.println("\n Offset Value from table: ");
-        String ofsValue = userInput.nextLine();
-
-        InputData newData =  new InputData(plainText, encodedString, ofsValue);
-
+        
+        while(choice != 3) {
+        	System.out.println("\n--------MENU-------- ");
+        	System.out.println("1. Encoder ");
+            System.out.println("2. Decoder ");
+            System.out.println("3. Exit ");
+            System.out.println("Select an option: ");
+            
+            choice = userChoice.nextInt();
+           
+            if(choice == 1) {
+            	System.out.print("String to Encode: ");
+                plainText = userInput.nextLine().toUpperCase();
+                
+                System.out.println("Offset Value from table: ");
+                ofsValue = userInput.nextLine().toUpperCase();
+                
+                newData.encoderData(plainText, ofsValue);
+                
+                EDService.ofsValue = newData.getOffset();
+                
+                System.out.println("Encoded String: ");
+                System.out.println(EDService.encode(newData.getPT()));
+                
+            }else if(choice == 2) {
+            	System.out.println("String to Decode: ");
+                encodedString = userInput.nextLine().toUpperCase();
+                
+                newData.decoderData(encodedString);
+                
+                System.out.println("Decoded String: ");
+                System.out.println(EDService.decode(newData.getES()));
+            }else if(choice !=3){
+            	System.out.println("Please enter a valid number (1/2/3).");
+            }
+        	
+        }
+        
         userInput.close();
-        EDService.ofsValue = newData.getOffset();
-
-        System.out.println("\nEncoded String: ");
-        System.out.println(EDService.encode(newData.getPT()));
-
-        System.out.println("\nDecoded String: ");
-        System.out.println(EDService.decode(newData.getES()));
+        userChoice.close();
+        System.exit(0);
 
     }
 
